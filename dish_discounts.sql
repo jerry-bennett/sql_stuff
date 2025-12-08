891b702f6c5cea7dc5fb5adc0dca369dff43de4f
Jerry Bennett <jerrbennett2@gmail.com>
2025-12-08 14:29:21 -0500

    Removed comments

--- dish_discounts.sql
+++ dish_discounts.sql
@@ -1,6 +1,5 @@
 -- SQL request(s)​​​​​​‌‌‌​‌​‌‌‌​‌‌​‌​​​‌‌‌‌‌​‌‌ below
 WITH DishDiscounts AS (
-    -- Step 1: Calculate the DiscountPercent in an inner query/CTE
     SELECT 
         Name AS Item,
         Price AS OriginalPrice,
@@ -8,16 +7,14 @@ WITH DishDiscounts AS (
             WHEN Price < 7 THEN 5
             WHEN Price >= 7 AND Price <= 10 THEN 10  
             WHEN Price > 10 THEN 15
-        END AS DiscountPercent -- This column is now safely defined
+        END AS DiscountPercent 
     FROM 
         Dishes
 )
--- Step 2: Use the result in the outer query to perform the division
 SELECT 
     Item,
     OriginalPrice,
     CONCAT(DiscountPercent, '%') AS DiscountPercent,
-    -- Perform the division using the previously defined column
     Round(OriginalPrice - (OriginalPrice * DiscountPercent / 100), 2) AS DiscountedPrice
 FROM 
     DishDiscounts
